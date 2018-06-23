b3b3bf34d3b4d999f0ba0b4a91ebdc6ccd73eded
kenta_okamura <side.junktown@gmail.com>
2018-06-23 13:17:28 +0900

    add install.go

---
+++ install.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os/exec"
+)
+
+var (
+	dryRun bool
+)
+
+func do(command string) {
+	fmt.Println("\x1b[36m❯", command, "\x1b[0m")
+	if dryRun {
+		return
+	}
+
+	out, err := exec.Command("sh", "-c", command).CombinedOutput()
+	if err != nil {
+		fmt.Println("\x1b[31mError:", err, "\x1b[0m")
+	}
+	fmt.Print(string(out))
+}
+
+func main() {
+	flag.BoolVar(&dryRun, "dry-run", false, "dry run")
+	flag.Parse()
+
+	if dryRun {
+		fmt.Println("dryRun enabled.")
+	}
+
+	dirs := []string{}
+	for _, dir := range dirs {
+		do("mkdir -p " + dir)
+	}
+
+	files := []string{
+		".vimrc",
+		".config/karabiner",
+	}
+	for _, file := range files {
+		do(fmt.Sprintf("ln -ins ~/projects/dotfiles/%s ~/%s", file, file))
+	}
+}
